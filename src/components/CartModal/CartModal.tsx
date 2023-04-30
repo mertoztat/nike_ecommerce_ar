c08761aad04a93c06d49c3ddec64307d7c3834e2
Mert Öztat <mertoztat@gmail.com>
2023-04-30 22:23:34 +0300

    feat: cart created

--- src/components/CartModal/CartModal.tsx
+++ src/components/CartModal/CartModal.tsx
@@ -1,7 +1,74 @@
 import "./CartModal.css";
+import { addToCart, deleteFromCart } from "../../features/slices/cartSlice";
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "features/hook";
+import ModelBox from "components/Models/ModelBox/ModelBox";
+import { FaTimes } from "react-icons/fa";
 
-const CartModal = () => {
-  return <div>CartModal</div>;
+export interface IProps {
+  data: any;
+  cartList: [];
+}
+
+const CartModal = ({ setIsCartOpen }: any) => {
+  const dispatch = useAppDispatch();
+  const cartList: IProps = useSelector((state: any) => state?.carts?.cartList);
+
+  const deleteFromCartButton = (product: any) => {
+    dispatch(deleteFromCart(product));
+  };
+
+  const addToCartButton = (product: any) => {
+    dispatch(addToCart(product));
+  };
+
+  const navbarCarts = Object.entries(cartList);
+
+  console.log("navbar cart", navbarCarts);
+
+  return (
+    <div className="cartWrapper">
+      <div className="closeBtn">
+        <h2>Cart: </h2>
+        <FaTimes onClick={() => setIsCartOpen(false)} />
+      </div>
+      {navbarCarts &&
+        navbarCarts?.map(([key, value]) => (
+          <>
+            <div className="cartItems">
+              <div className="cartTopItems">
+                <ModelBox
+                  width="8em"
+                  height="8 em"
+                  glbSrc={value.glbFile}
+                  iosSrc={value.iosSrc}
+                />
+                <p>{value.name}</p>
+                <div className="counter">
+                  <div className="counterQuantity">
+                    <span
+                      onClick={() => deleteFromCartButton(value)}
+                      className="decrease"
+                    >
+                      -
+                    </span>
+                    <span className="count">{value.quantity}</span>
+                    <span
+                      onClick={() => addToCartButton(value)}
+                      className="increase"
+                    >
+                      +
+                    </span>
+                  </div>
+                  <span>{value.quantity * value.price}</span>
+                  {/* <button>Satın Al</button> */}
+                </div>
+              </div>
+            </div>
+          </>
+        ))}
+    </div>
+  );
 };
 
 export default CartModal;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,34 +2,39 @@ import "./Navbar.css";
 import { FaHeart, FaShoppingCart, FaSearch } from "react-icons/fa";
 import logo from "assets/logo.png";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import CartModal from "components/CartModal/CartModal";
 
 const Navbar = () => {
+  const [isCartOpen, setIsCartOpen] = useState<boolean>(false);
   return (
-    <header>
-      <nav>
-        <div className="logo">
-          <img src={logo} alt="nike_logo" />
-        </div>
-        <div className="nav_search">
-          <input type="text" placeholder="Search" />
-          <FaSearch size={15} className="search_icon" />
-        </div>
-        <div className="nav_items">
-          <ul>
-            <li className="cart_fav">
-              <FaHeart size={20} />
-            </li>
-            <li className="cart_item">
-              <FaShoppingCart
-                // onClick={() => dispatch(setIsOpenCart({}))}
-                // onClick={() => dispatch(setIsOpenCart({ isCartOpen: !cart }))}
-                size={20}
-              />
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav>
+          <div className="logo">
+            <img src={logo} alt="nike_logo" />
+          </div>
+          <div className="nav_search">
+            <input type="text" placeholder="Search" />
+            <FaSearch size={15} className="search_icon" />
+          </div>
+          <div className="nav_items">
+            <ul>
+              <li className="cart_fav">
+                <FaHeart size={20} />
+              </li>
+              <li className="cart_item">
+                <FaShoppingCart
+                  onClick={() => setIsCartOpen(!isCartOpen)}
+                  size={20}
+                />
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      {isCartOpen && <CartModal setIsCartOpen={setIsCartOpen} />}
+    </>
   );
 };
 
