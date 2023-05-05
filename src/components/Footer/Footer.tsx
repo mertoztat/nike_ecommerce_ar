57b77cd58e10a43e08d54aecc1916d1db8a1f6c8
Mert Öztat <mertoztat@gmail.com>
2023-05-05 16:46:14 +0300

    feat: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,46 +5,70 @@ import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer_logo">
-        <img src={logo} alt="nike_logo" />
-      </div>
-      <div className="footer_desc">
-        <h5>Designed & Developed by</h5>
-        <div className="footer_item">
-          <a href="https://reactfiberportfoliowebsite.vercel.app/">
-            Hanife Erkalan
-          </a>{" "}
-          & <a href="https://mertoztat.dev/">Mert Öztat</a>
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-row">
+          <div className="footer-col">
+            <h4>About Nike</h4>
+            <ul>
+              <li>
+                <a href="#">about us</a>
+              </li>
+              <li>
+                <a href="#">our services</a>
+              </li>
+              <li>
+                <a href="#">privacy policy</a>
+              </li>
+              <li>
+                <a href="#">Careers</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>get help</h4>
+            <ul>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">shipping</a>
+              </li>
+              <li>
+                <a href="#">returns</a>
+              </li>
+              <li>
+                <a href="#">order status</a>
+              </li>
+              <li>
+                <a href="#">payment options</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>follow us</h4>
+            <div className="social-links">
+              <div className="social-items">
+                <span>
+                  <FaFacebook size={30} />
+                </span>
+                <span>
+                  <FaTwitter size={30} />
+                </span>
+              </div>
+              <div className="social-items">
+                <span>
+                  <FaInstagram size={30} />
+                </span>
+                <span>
+                  <FaYoutube size={30} />
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <h5>&copy; {new Date().getFullYear()} </h5>
       </div>
-      <div className="footer_social">
-        <ul>
-          <li>
-            <a href="https://www.facebook.com/nike">
-              <FaFacebook size={25} />
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/Nike">
-              <FaTwitter size={25} />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/nike/">
-              <FaInstagram size={25} />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.youtube.com/user/nike">
-              <FaYoutube size={30} />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </footer>
   );
 };
 
